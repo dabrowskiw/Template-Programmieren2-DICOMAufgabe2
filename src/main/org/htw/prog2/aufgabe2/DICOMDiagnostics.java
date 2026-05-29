adf3745e89c568923eed96747b29ca6ddfcc28b2
Piotr Wojciech Dabrowski <piotr.dabrowski@htw-berlin.de>
2026-05-29 13:30:27 +0200

    Changed solutions to pixelmed

--- src/main/org/htw/prog2/aufgabe2/DICOMDiagnostics.java
+++ src/main/org/htw/prog2/aufgabe2/DICOMDiagnostics.java
@@ -7,8 +7,8 @@ import java.io.File;
 public class DICOMDiagnostics {
 
     public static void main(String[] args) {
-//        DICOMImage image = new DICOMImage(new File("data/angiogram1.DCM"), "Angiogram");
-//        image.writeFrames(50, 60, false, true,10);
+        DICOMImage image = new DICOMImage(new File("data/angiogram1.DCM"), "Angiogram");
+        image.writeFrames(50, 60, false, true,10);
         MainFrame main = new MainFrame();
         main.setVisible(true);
     }
